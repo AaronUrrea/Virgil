8a9ab30d677f0497a64a85b8ad0a5fbaadefc195
AaronUrrea <aaronurrea343@gmail.com>
2020-06-29 23:08:57 -0700

    Edit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
-
 client.on('ready', async () =>{
     console.log("This client is online \n");
     
