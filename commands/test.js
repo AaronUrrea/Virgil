30fadd18ad4030fb7ac22d3d73dc733cb0734ef0
AaronUrrea <aaronurrea343@gmail.com>
2020-06-30 15:11:14 -0700

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,11 +5,28 @@ const config = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
     if(message.member.permissions.toArray().includes('ADMINISTRATOR')){
-        message.channel.send(message.member.nickname + ` has a large pp`);
+        let sent = await message.channel.send(new Discord.MessageEmbed()
+            .setColor('#ff9900')
+            .setTitle('111th Manticore Company')
+            .setDescription('Permission granted, ' + message.member.nickname + '.\nYou have quite a large PP.')
+            .attachFiles(['./attachments/UNSC.png', './attachments/Manticore.png'])
+            .setAuthor('UNSC', 'attachment://UNSC.png')
+            .setThumbnail('attachment://Manticore.png'));
+        
+         await message.delete();
+
     console.log("[Virgil @ " + message.channel.name + "]: " + message.toString());
     }
     else{
-        message.reply("You have insufficent permissions for this command.");
+        let sent = await message.channel.send(new Discord.MessageEmbed()
+            .setColor('#ff9900')
+            .setTitle('111th Manticore Company')
+            .setDescription('Permission denied, ' + message.member.nickname + '.\nYou have quite a small PP.')
+            .attachFiles(['./attachments/UNSC.png', './attachments/Manticore.png'])
+            .setAuthor('UNSC', 'attachment://UNSC.png')
+            .setThumbnail('attachment://Manticore.png'));
+        
+        await message.delete();
     }
 }
 
